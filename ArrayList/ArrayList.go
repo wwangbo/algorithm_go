c850586531e77fc29383af26c07ab06fe65acbbb
wwangbo <1940844088@qq.com>
2020-06-01 15:22:13 +0800

    初步完成数组相关功能

--- ArrayList/ArrayList.go
+++ ArrayList/ArrayList.go
@@ -19,17 +19,19 @@ type List interface {
 
 //数据结构
 type ArrayList struct {
-	dataStore [] interface{} //范型，存储数据
+	dataStore []interface{} //范型，存储数据
 	size int //数组大小
 }
 
-func NewArrayList()*ArrayList{
+func NewArrayList() *ArrayList{
 	list := new(ArrayList)
 	list.dataStore = make([]interface{},0,10)
 	list.size = 0
 	return list
 }
-
+func (list *ArrayList) String() string{
+	return fmt.Sprint(list.dataStore)
+}
 func (list *ArrayList)Size() int  {
 	return list.size  //返回数组的大小
 }
@@ -41,11 +43,47 @@ func (list *ArrayList) Get(index int) (interface{},error) {
 	return list.dataStore[index],nil
 }
 
+func (list *ArrayList) Set(index int,newVal interface{}) error {
+	if index < 0 || index >= list.size {
+		return errors.New("索引越界")
+	}
+	list.dataStore[index] = newVal
+	return nil
+}
+
+func (list *ArrayList)Insert(index int,newVal interface{}) error  {
+	if index < 0 || index >= list.size {
+		return errors.New("索引越界")
+	}
+	list.checkisFull()//检测内存，如果满了自动扩容
+	list.dataStore=list.dataStore[:list.size+1]
+	for i := list.size;i>index;i-- { //从后往前移动
+		list.dataStore[i] = list.dataStore[i-1]
+	}
+	list.dataStore[index]=newVal
+	list.size++
+	return nil
+}
 func (list *ArrayList)Append(newVal interface{}) {
 	list.dataStore = append(list.dataStore,newVal)
 	list.size++
 }
+func (list *ArrayList) Clear(){
+	list.dataStore = append(list.dataStore,0,10)
+	list.size=0
+}
 
-func (list *ArrayList) String() string{
-	return fmt.Sprint(list)
+func (list *ArrayList) Delete(index int) error  {
+	list.dataStore=append(list.dataStore[:index],list.dataStore[index+1:]...)
+	list.size--
+	return nil
+}
+
+
+func (list *ArrayList) checkisFull()  {
+	if list.size==cap(list.dataStore){//内存已经使用完
+		newdataStore := make([]interface{},0,2*list.size)
+		copy(newdataStore,list.dataStore)
+		list.dataStore=newdataStore
+	}
 }
